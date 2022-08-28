4d7e8890ebc84780e789952154d8fae32f817275
Fostinos <kafostin@gmail.com>
2022-08-28 09:08:08 +0100

    Notion de fonction

---
+++ function/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+
+int auCarre(int param); // Declaration de prototype
+
+int main()
+{
+	return 0;
+}
+
+int auCarre(int param)
+{
+	// Implementation
+	int carre;
+	carre = param * param;
+	return carre;
+}
